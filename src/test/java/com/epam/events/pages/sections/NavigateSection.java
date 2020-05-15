4d1265e4f3df94f40f315ee1c7667d3e1902f912
Konovalov Semen <s.konovalov@hostco.ru>
2020-05-15 22:59:04 +0500

    feature: add navigate section for PageObject

---
+++ src/test/java/com/epam/events/pages/sections/NavigateSection.java
@@ -0,0 +1,48 @@
+package com.epam.events.pages.sections;
+
+import com.epam.events.config.ServerConfig;
+import com.epam.events.utils.DriversManager;
+import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NavigateSection {
+    public static ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+    final public static Logger logger = LogManager.getLogger(DriversManager.class);
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @FindBy(xpath = "//a[@href='/events']")
+    private WebElement eventsBtn;
+
+    @FindBy(xpath = "//a[@href='/talks']")
+    private WebElement talksLibraryBtn;
+
+    public NavigateSection(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+        PageFactory.initElements(driver, this);
+    }
+
+    public NavigateSection clickEventsBtn() {
+        eventsBtn.click();
+
+        PageFactory.initElements(driver, this);
+        return this;
+    }
+
+    public NavigateSection clickTalksLibraryBtn() {
+        talksLibraryBtn.click();
+
+        PageFactory.initElements(driver, this);
+        return this;
+    }
+
+}
